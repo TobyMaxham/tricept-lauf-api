0bc23e107ce0804e9483e42da6f07a4e93afc6f0
Tobias Maxham <tobias.maxham@tricept.de>
2021-07-05 22:39:13 +0200

    pull daily repository changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('username');
-            $table->string('app_token');
+            $table->text('app_token')->nullable();
             $table->timestamp('token_created')->nullable();
             //$table->string('email')->unique();
             //$table->timestamp('email_verified_at')->nullable();
